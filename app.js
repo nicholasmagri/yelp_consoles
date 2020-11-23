d2821120c260b565069f0471240a73e83b27c304
Nicholas Magri <nicholas.magri@coker.edu>
2020-11-23 17:01:18 +0000

    Add flash messages to res.locals

--- app.js
+++ app.js
@@ -85,9 +85,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 passport.use(new LocalStrategy(User.authenticate()));
 
-// Current User Middleware Config
+// State Config
 app.use((req, res, next) => {
 	res.locals.user = req.user;
+	res.locals.errorMessage = req.flash("error");
+	res.locals.successMessage = req.flash("success");
 	next();
 })
 
